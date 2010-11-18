358719b4289d8ff13252ccb6d5268f220491a563
chrisroberts <chrisroberts@colortechnology.com>
2010-11-18 11:06:55 -0800

    Options for using IFrame or not

--- lib/window_rails/window_rails_view.rb
+++ lib/window_rails/window_rails_view.rb
@@ -1,12 +1,14 @@
 module WindowRailsView
   
   # name:: Name of the link
-  # options:: Hash of optional values. These options are simply passed on to the activate_window method
-  # Creates a link whose contents will be loaded into a window. Currently loaded content is within
-  # an IFrame, which means that links within the window will not have the ability to communicate with
-  # the DOM of the page around it. (This means you cannot close the window from a link within it)
+  # options:: Hash of options values.
+  # Creates a link to a window. Content is defined by a url in the options hash using :url or :iframe. 
+  # If :url is used, the content is loaded into the window within the page. If :iframe is used
+  # the content is loaded into the window within an IFrame on the page. Generally, if you are calling
+  # a method that simply renders out a partial, you want to use :url. If you are calling something
+  # that returns an entire page, :iframe will likely be the ticket.
   def link_to_window(name, options={})
-    link_to_remote(name, :url => {:controller => :window_rails, :action => :open_window, :window_url => url_for(options.delete(:url)), :window_options => options})
+    link_to_remote(name, :url => {:controller => :window_rails, :action => :open_window, :window_url => url_for(options.delete(:url)), :iframe_url => url_for(options.delete(:iframe)), :window_options => options})
   end
   
 end
