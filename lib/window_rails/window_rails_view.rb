11888ad0cf2519326bc58bef52d56c1e8b9f17bb
chrisroberts <chrisroberts@colortechnology.com>
2011-05-12 20:29:58 -0700

    Update #link_to_window to accept html options

--- lib/window_rails/window_rails_view.rb
+++ lib/window_rails/window_rails_view.rb
@@ -7,8 +7,19 @@ module WindowRailsView
   # the content is loaded into the window within an IFrame on the page. Generally, if you are calling
   # a method that simply renders out a partial, you want to use :url. If you are calling something
   # that returns an entire page, :iframe will likely be the ticket.
-  def link_to_window(name, options={})
-    link_to_remote(name, :url => {:controller => :window_rails, :action => :open_window, :window_url => url_for(options.delete(:url)), :iframe_url => url_for(options.delete(:iframe)), :window_options => options})
+  def link_to_window(name, options={}, html_opts={})
+    window_url = options[:url] ? url_for(options.delete(:url)) : nil
+    options[:iframe] = options.delete(:iframe_url) if options[:iframe_url]
+    link_to_remote(
+      name, {
+      :url => {
+        :controller => :window_rails,
+        :action => :open_window,
+        :window_url => window_url,
+        :window_options => options
+      }},
+      html_opts
+    )
   end
   
 end
