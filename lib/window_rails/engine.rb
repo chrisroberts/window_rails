485d26055cd3b9ecfce44272db8c272f516a7179
Chris Roberts <chrisroberts.code@gmail.com>
2011-07-12 16:13:43 -0700

    Prep for 0.2 release

--- lib/window_rails/engine.rb
+++ lib/window_rails/engine.rb
@@ -10,16 +10,6 @@ module WindowRails
       %w(window_rails_generators window_rails_view).each do |part|
         require "window_rails/#{part}"
       end
-      ActionView::Helpers::AssetTagHelper.register_javascript_expansion(
-        :plugins => Dir.glob(
-          File.join(Rails.root, 'public', 'window_rails', 'javascripts', '*.js')
-        ).map{|file| file.sub(File.join(Rails.root, 'public'), '')}
-      )
-      ActionView::Helpers::AssetTagHelper.register_stylesheet_expansion(
-        :plugins => Dir.glob(
-          File.join(Rails.root, 'public', 'window_rails', 'stylesheets', '*.css')
-        ).map{|file| file.sub(File.join(Rails.root, 'public'), '')}
-      )
     end
   end
 end
