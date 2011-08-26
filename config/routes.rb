83e56c343e590a1b9ee478f2735da9ad7ad84df3
Chris Roberts <chrisroberts.code@gmail.com>
2011-08-26 13:49:57 -0700

    Update routing for rails 3

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 ActionController::Routing::Routes.draw do |map|
-  map.open_window 'window_rails/open_window', :controller => :window_rails, :action => :open_window
+  if(Rails.version.split('.').first.to_i < 3)
+    map.open_window 'window_rails/open_window', :controller => :window_rails, :action => :open_window
+  else
+    match 'window_rails/open_window' => 'window_rails#open_window'
+  end
 end
