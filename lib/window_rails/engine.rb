0bfb35be4a0eedc991655aba699666e3484176ec
Chris Roberts <chrisroberts.code@gmail.com>
2012-01-09 10:40:08 -0800

    Use correct path

--- lib/window_rails/engine.rb
+++ lib/window_rails/engine.rb
@@ -8,7 +8,7 @@ module WindowRails
     # We do all our setup in here
     config.to_prepare do
       if(defined?(ActionView::Helpers::PrototypeHelper::JavaScriptGenerator::GeneratorMethods))
-        require 'window_rails_generators'
+        require 'window_rails/window_rails_generators'
       end
       %w(window_rails_windows window_rails_view).each do |part|
         require "window_rails/#{part}"
