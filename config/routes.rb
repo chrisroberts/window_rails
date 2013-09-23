e6b2c2d206787be3e6f6eb4ec9ad9658c52caea0
Chris Roberts <chrisroberts.code@gmail.com>
2013-09-23 09:02:28 -0700

    Update routing so rails 4 doesn't get crabby

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ if(Rails.version.split('.').first.to_i < 3)
   end
 else
   Rails.application.routes.draw do
-    match 'window_rails/open_window' => 'window_rails#open_window', :as => :open_window
+    if(Rails.version.split('.').first.to_i < 4)
+      match 'window_rails/open_window' => 'window_rails#open_window', :as => :open_window
+    else
+      get 'window_rails/open_window', to: 'window_rails#open_window', :as => :open_window
+    end
   end
 end
