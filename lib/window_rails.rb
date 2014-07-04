84bc05266310b29783afce3fb485b03adade5c02
Chris Roberts <chrisroberts.code@gmail.com>
2014-07-04 11:11:28 -0700

    Include generators load

--- lib/window_rails.rb
+++ lib/window_rails.rb
@@ -5,6 +5,7 @@ module WindowRails
   autoload :Engine, 'window_rails/engine'
   autoload :Windows, 'window_rails/windows'
   autoload :Holder, 'window_rails/holder'
+  autoload :Generators, 'window_rails/generators'
 end
 
 require 'window_rails/engine'
